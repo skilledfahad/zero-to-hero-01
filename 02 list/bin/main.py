32066aa7f3539dfd7b78c50fff8b28f47ba3df9c
fahad <fahad.itwork@gmail.com>
2022-10-24 00:49:08 +0600

    renamed:    01 Data type & opperator/bin/var_&_opperator.py -> 01 Data type & opperator/bin/main.py 	new file:   02 list/bin/main.py 	renamed:    Current_balance(var,opperator)/bin/account.py -> P01. Current_balance(var,opperator)/bin/account.py 	renamed:    Current_balance(var,opperator)/bin/main.dart -> P01. Current_balance(var,opperator)/bin/main.dart 	new file:   P02. student_list/main.py

---
+++ 02 list/bin/main.py
@@ -0,0 +1,12 @@
+from operator import indexOf
+
+
+student=["Arif","Bosir","Chohan"]
+
+student.insert(3,"Denas") #student.insert(INDEX_NUM,"Denas")
+student.insert(4,34)
+student.append("sohan")
+
+print(student)
+print("List item no:: 4 = " + str(student[4]))
+
---
+++ P02. student_list/main.py
@@ -0,0 +1,23 @@
+students2022=["arif","Borhan","Chami","Dalim"]
+print(students2022)
+
+# Single input
+new_student_add=input("Input new student name :: ")
+students2022.append(new_student_add)
+
+print(students2022)
+
+# Single remove
+student_left=input("Student who left ::")
+students2022.remove(student_left)
+
+print(students2022)
+
+
+
+
+
+
+
+
+
