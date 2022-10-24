204aa816bbe0c27f6129a22f862b4d5a66830018
fahad <fahad.itwork@gmail.com>
2022-10-24 23:43:10 +0600

    modified:   02 list/bin/main.dart 	modified:   02 list/bin/main.py

--- 02 list/bin/main.dart
+++ 02 list/bin/main.dart
@@ -1,53 +1,57 @@
 void main() {
   // fixed length list
   print("*****Fixed length list*****");
-  var list2 = List.filled(6, 0, growable: false);
-  list2[0] = 1;
-  list2[1] = 2;
-  list2[2] = 3;
-  list2[3] = 4;
-  list2[4] = 5;
-  list2[5] = 6;
-  list2.add(98);
+  var list2 = List.filled(6, "A", growable: true);
+  list2[0] = "arif";
+  list2[1] = "Bosir";
+  list2[2] = "cosir";
+  list2[3] = "dosir";
+  list2[4] = "eosir";
+  list2[5] = "fosir";
+
+  list2.add("Gosir");
 
   print(list2);
 
-  // GrowableList
-  print("****Growable list****");
-  List<int> list1 = [];
+  // To check whether, and where, the element is in the list, use indexOf
+  print(list2.indexOf("cosir"));
 
-  list1.add(1);
-  list1.add(2);
-  list1.add(3);
-  list1.add(4);
-  list1.add(5);
+  // // GrowableList
+  // print("****Growable list****");
+  // List<int> list1 = [];
 
-  print(list1);
-  print("Print index 4");
-  print(list1[4]);
+  // list1.add(1);
+  // list1.add(2);
+  // list1.add(3);
+  // list1.add(4);
+  // list1.add(5);
 
-  print("Print index 4= ${list1[4]}");
+  // print(list1);
+  // print("Print index 4");
+  // print(list1[4]);
 
-  list1.remove(2);
-  print("Remove 2");
-  print(list1);
+  // print("Print index 4= ${list1[4]}");
 
-  list1.removeAt(2);
-  print("Remove list index 2");
-  print(list1);
+  // list1.remove(2);
+  // print("Remove 2");
+  // print(list1);
 
-  list1.removeRange(0, 2);
-  print("Remove Index {star 0 to end 2}");
-  print(list1);
+  // list1.removeAt(2);
+  // print("Remove list index 2");
+  // print(list1);
 
-  list1.addAll([1, 2, 3, 4]);
-  print("Add some data");
-  print(list1);
+  // list1.removeRange(0, 2);
+  // print("Remove Index {star 0 to end 2}");
+  // print(list1);
 
-  // ###string###
-  var listS = [];
-  listS.addAll(["bosir", "mohon", "maria"]);
-  print(listS);
+  // list1.addAll([1, 2, 3, 4]);
+  // print("Add some data");
+  // print(list1);
 
-  print("printing list length ${listS.length}");
+  // // ###string###
+  // var listS = [];
+  // listS.addAll(["bosir", "mohon", "maria"]);
+  // print(listS);
+
+  // print("printing list length ${listS.length}");
 }
--- 02 list/bin/main.py
+++ 02 list/bin/main.py
@@ -2,17 +2,19 @@
 student=["Arif","Bosir","Chohan"]
 print(student)
 
+# adding singel data
+student[1]="one"
+student.insert(3,"Denas") #student.insert(INDEX_NUM,"Denas")
+student.append("Sohan")
+print(student)
+
+print("List item no:: 4 = " + str(student[4]))
+
 # way 2
-thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+thislist = list(("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")) # note the double round-brackets
 print(thislist)
 
-# adding singel data
-student.insert(3,"Denas") #student.insert(INDEX_NUM,"Denas")
-student.insert(4,34)
-student.append("Sohan")
-
-print(student)
-print("List item no:: 4 = " + str(student[4]))
+print(thislist[0:-4])
 
 # adding many data
 
